4cf494fe77322e166c9f8a4d754828261be59f14
Ayomikun <olalekanayomikun03@gmail.com>
2023-01-08 00:22:18 -0800

    fourth commit

--- script.js
+++ script.js
@@ -2,9 +2,6 @@ const mainMenu = document.querySelector('.mainMenu');
 const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.openMenu');
 const match = document.querySelector('.match');
-const boys = document.querySelector('.boys')
-const girl = document.querySelector('.girl')
-const either = document.querySelector('.either')
 
 
 openMenu.addEventListener('click',show);
@@ -21,35 +18,4 @@ function close(){
 match.onclick = function(e){
     e.preventDefault();
     location.href = '/sponsor.html'
-}
-
-
-// girl.onclick = function(){
-//     girl.style.background = 'blue'
-//     girl.style.color = 'white'
-// }
-
-// boys.onclick = function(){
-//     boys.style.background = 'blue'
-//     boys.style.color = 'white'
-// }
-
-// either.onclick = function(){
-//     either.style.background = 'blue'
-//     either.style.color = 'white'
-// }
-
-
-
-let form = document.forms['my-form'];
-console.log(form)
-let menu = form.country;
-let options = form.country.options;
-
-form.onsubmit = function(e){
-    e.preventDefault();
-
-    let optionValue = this.country.value;
-
-    // location.href = '/sponsor.html'
 }
